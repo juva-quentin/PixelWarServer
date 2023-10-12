be06c7f67dfdd68523d997c6002bfc34848a582c
Quentin JUVET <quentinjuvet@MacBook-Pro-de-Quentin.local>
2023-10-12 10:25:37 +0200

    first commit

---
+++ src/main/java/com/example/pixelwarserver/PixelWarServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pixelwarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelWarServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelWarServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pixelwarserver/PixelWarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pixelwarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelWarServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
