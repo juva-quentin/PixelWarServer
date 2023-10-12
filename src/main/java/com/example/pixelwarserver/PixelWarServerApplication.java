673d6280a2364fd0d4743f2135218900fb254ae7
juva-quentin <quentin.juvet@ynov.com>
2023-10-12 16:54:06 +0200

    add env variable

--- src/main/java/com/example/pixelwarserver/PixelWarServerApplication.java
+++ src/main/java/com/example/pixelwarserver/PixelWarServerApplication.java
@@ -4,24 +4,29 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 /**
  * Application principale de PixelWar Server.
  * Cette classe démarre l'application Spring Boot et configure la connexion MongoDB,
  * ainsi que la configuration WebSocket.
  */
+
 @EnableMongoRepositories(basePackages = "com.example.pixelwarserver.repository")
 @SpringBootApplication
 public class PixelWarServerApplication {
+
+    @Value("${database.name}")
+    private String databaseName;
+
+    @Value("${database.pswd}")
+    private String databasePswd;
 
     /**
      * Point d'entrée de l'application.
@@ -39,7 +44,7 @@ public class PixelWarServerApplication {
      */
     @Bean
     public MongoClient mongoClient() {
-        String connectionString = "mongodb+srv://juvetquentin19:pHwT4UVrlS0xXOPv@cluster0.ouqpp0e.mongodb.net/?retryWrites=true&w=majority";
+        String connectionString = "mongodb+srv://" + databaseName + ":" + databasePswd + "@cluster0.ouqpp0e.mongodb.net/?retryWrites=true&w=majority";
 
         ConnectionString connectionStr = new ConnectionString(connectionString);
 
