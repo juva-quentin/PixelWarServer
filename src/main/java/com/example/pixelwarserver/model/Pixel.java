812909ade46b77d3876fdf16c7f5688cd567f1f2
juva-quentin <quentin.juvet@ynov.com>
2023-10-12 16:24:30 +0200

    added obj pixel with getter and setter

---
+++ src/main/java/com/example/pixelwarserver/model/Pixel.java
@@ -0,0 +1,60 @@
+package com.example.pixelwarserver.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "pixels")
+public class Pixel {
+    @Id
+    private String id;
+    private int x;
+    private int y;
+    private String color;
+
+    public Pixel() {
+    }
+
+    public Pixel(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    // Getters et setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
